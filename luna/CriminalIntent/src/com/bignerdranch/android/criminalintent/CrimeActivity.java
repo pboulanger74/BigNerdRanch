ee746d1c674bc67ddfe356cc5ebdc7bfdbdf058a
Patrick Boulanger <patrickboulanger@yahoo.com>
2015-01-28 23:06:11 +0100

    some test

---
+++ luna/CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CrimeActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime);
+	}
+}
